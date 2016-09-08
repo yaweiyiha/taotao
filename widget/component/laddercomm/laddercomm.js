918ef403ed52a7915a7cd43ab66a02c305ca48ad
xiaolin3303 <xiaolin3303@163.com>
2016-09-08 20:50:23 +0800

    ladder

--- widget/component/laddercomm/laddercomm.js
+++ widget/component/laddercomm/laddercomm.js
@@ -53,6 +53,7 @@ export default Vue.component('ladder-comm', {
  					cache.maxExtraRate = +item.upperProportion;
  				} else {
 	 				cache.extraCommission = +item.proportion;
+	 				cache.extraRate = +item.proportion;
  				}
  				cache.measureUnitFk = this.unit ? dict[this.unit] : dict[item.unit];
  				cache.sorter = index;
