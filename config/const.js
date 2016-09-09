d5603260bcfe04fe5dbf469c367f917f1dc2f3fe
xiaolin3303 <xiaolin3303@163.com>
2016-09-09 12:27:11 +0800

    d

--- config/const.js
+++ config/const.js
@@ -2,4 +2,4 @@ const INTERFACE_FATAL_ERROR = 'Interface Fatal Error';
 // const URL_MODE = !!history.pushState ? 'pushstate' : 'hash';
 // const URL_MODE = 'hash';
 const URL_MODE = 'hash';
-const ROOT = 'admin';
+const ROOT = '';
